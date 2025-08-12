a05dfb23f499ea6f68c3934d5fbb3064c1f56551
Panupong thongdee <pranupong.49@gmail.com>
2025-08-13 00:28:56 +0700

    ENGCC304

---
+++ Home_work/Hw1/HW1-3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+char first [10];
+char last [10];
+
+int main ()
+{
+   printf("Enter your First \n");
+   scanf("%s", &first);
+   printf("%s'\n","----------------");
+   printf("Enter your last \n");
+   scanf(" %s", &last);
+
+   printf("%s %s IC RMUTL Chingmai Thailand",first,last);
+   
+
+    return 0;
+}
