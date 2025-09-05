005790f3f057b07351a701b685907874f5d7b417
Panupong thongdee <pranupong.49@gmail.com>
2025-09-05 11:53:36 +0700

    ENGCC304

---
+++ Home_work/Hw6/Hw6-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+  int num ;
+  int i ;
+  
+ printf("ENTER NUMBER \n");
+ scanf("%d",&i);
+  
+  for( num=1  ; num <= i ; num++){
+
+     printf("( %d ) Hello world \n", num );
+  }
+
+  return 0 ;
+}
