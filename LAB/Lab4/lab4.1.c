ac30e4ead624b5151f2340c5703d11f332d36335
Panupong thongdee <pranupong.49@gmail.com>
2025-10-11 14:43:26 +0700

    ENGCC304

---
+++ LAB/Lab4/lab4.1.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int input_day ;
+int main() {
+printf("Enter Day :");
+scanf("%d",&input_day);
+
+int sum ;
+sum = input_day * 86400;
+printf("%d",sum);
+
+    return 0;
+}
